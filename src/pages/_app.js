493dd3be1c5ddc47b96fa5fc316bd7b1a81a0bc9
Victor Anyadioha <victoranyadioha@gmail.com>
2023-02-08 14:39:40 +0100

    Style: Added fonts and Global styling to _app.jsx

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,57 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Plus_Jakarta_Sans as PJSans } from "@next/font/google";
 
+const pjsans = PJSans({ subsets: ["latin"] });
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        *,
+        *::before,
+        *::after {
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+        }
+        ul {
+          list-style: none;
+        }
+        a {
+          text-decoration: none;
+        }
+        html {
+          font-size: 0.625em;
+        }
+        body {
+          font-size: 1.6rem;
+          font-family: ${pjsans.style.fontFamily};
+          overflow-x: hidden;
+          background-color: #121128;
+          color: #fff;
+        }
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          object-position: center;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          font-weight: 700;
+        }
+        button {
+          font-family: ${pjsans.style.fontFamily};
+          font-size: 1.6rem;
+        }
+        p {
+          font-weight: 500;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
